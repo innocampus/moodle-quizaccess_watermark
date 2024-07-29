c5566fa71a64c8db0375661c0dc22c2a7e5806e4
Daniil Fajnberg <d.fajnberg@tu-berlin.de>
2024-07-29 11:59:09 +0200

    Clean up `rule.php` (fixes #7)
    - Use null-coalescing for `watermark_enabled` property
    - Replace redundant PHPDocs on methods with `@inheritDoc` tags
    - Add return types to methods
    - Add missing `@throws` tags to methods
    - Slightly improve readabiltiy in a few places

--- rule.php
+++ rule.php
@@ -37,15 +37,10 @@ defined('MOODLE_INTERNAL') || die();
 class quizaccess_watermark extends access_rule_base {
 
     /**
-     * Return an appropriately configured instance of this rule, if it is applicable
-     * to the given quiz, otherwise return null.
-     * @param quiz_settings $quizobj information about the quiz in question.
-     * @param int $timenow the time that should be considered as 'now'.
-     * @param bool $canignoretimelimits whether the current user is exempt from
-     *      time limits by the mod/quiz:ignoretimelimits capability.
-     * @return access_rule_base|null the rule, if applicable, else null.
+     * @inheritDoc
+     * @return self|null the rule, if applicable, else null.
      */
-    public static function make(quiz_settings $quizobj, $timenow, $canignoretimelimits) {
+    public static function make(quiz_settings $quizobj, $timenow, $canignoretimelimits): ?self {
         if ($quizobj->get_quiz()->watermark_enabled ?? false) {
             return new self($quizobj, $timenow);
         }
@@ -53,67 +48,66 @@ class quizaccess_watermark extends access_rule_base {
     }
 
     /**
-     * Sets up the attempt (review or summary) page with any special extra
-     * properties required by this rule. securewindow rule is an example of where
-     * this is used.
-     *
-     * @param moodle_page $page the page object to initialise.
+     * @inheritDoc
+     * @throws dml_exception
      */
-    public function setup_attempt_page($page) {
+    public function setup_attempt_page($page): void {
         global $USER;
 
         $settings = get_config('quizaccess_watermark');
 
         $teacher = $this->quizobj->has_capability('mod/quiz:viewreports');
         $userhash = quizaccess_watermark\manager::get_user_hash($teacher, $this->quizobj->get_quizid(), $USER->id);
-        $page->requires->js_call_amd('quizaccess_watermark/watermark-lazy', 'init', [
-            $teacher, $userhash, $settings->background_color, $settings->start_color, $settings->bit_color
-        ]);
+        $page->requires->js_call_amd(
+            'quizaccess_watermark/watermark-lazy',
+            'init',
+            [$teacher, $userhash, $settings->background_color, $settings->start_color, $settings->bit_color],
+        );
     }
 
-    public function is_preflight_check_required($attemptid) {
+    /**
+     * @inheritDoc
+     */
+    public function is_preflight_check_required($attemptid): bool {
         // Warning only required if the attempt is not already started.
         return $attemptid === null;
     }
 
-    public function add_preflight_check_form_fields(preflight_check_form $quizform,
-            MoodleQuickForm $mform, $attemptid) {
-
-        $mform->addElement('header', 'watermarkheader',
-                get_string('settings_header', 'quizaccess_watermark'));
-        $mform->addElement('static', 'watermarkmessage', '',
-                get_string('preflight_text', 'quizaccess_watermark'));
+    /**
+     * @inheritDoc
+     * @throws coding_exception
+     */
+    public function add_preflight_check_form_fields(preflight_check_form $quizform, MoodleQuickForm $mform, $attemptid): void {
+        $mform->addElement('header', 'watermarkheader', get_string('settings_header', 'quizaccess_watermark'));
+        $mform->addElement('static', 'watermarkmessage', '', get_string('preflight_text', 'quizaccess_watermark'));
     }
 
     /**
-     * Add any fields that this rule requires to the quiz settings form. This
-     * method is called from {@link mod_quiz_mod_form::definition()}, while the
-     * security seciton is being built.
-     * @param mod_quiz_mod_form $quizform the quiz settings form that is being built.
-     * @param MoodleQuickForm $mform the wrapped MoodleQuickForm.
+     * @inheritDoc
+     * @throws coding_exception
      */
-    public static function add_settings_form_fields(
-            mod_quiz_mod_form $quizform, MoodleQuickForm $mform) {
-
-        if (has_capability('quizaccess/watermark:manage_quiz_settings', $quizform->get_context())) {
-            $header = $mform->createElement('header', 'watermark',
-                    get_string('settings_header', 'quizaccess_watermark'));
-            $mform->insertElementBefore($header, 'security');
-
-            $option = $mform->createElement('selectyesno', 'watermark_enabled',
-                    get_string('settings_watermark_enable', 'quizaccess_watermark'));
-            $mform->setDefault('watermark_enabled', 0);
-            $mform->insertElementBefore($option, 'security');
+    public static function add_settings_form_fields(mod_quiz_mod_form $quizform, MoodleQuickForm $mform): void {
+        if (!has_capability('quizaccess/watermark:manage_quiz_settings', $quizform->get_context())) {
+            return;
         }
+        $header = $mform->createElement('header', 'watermark', get_string('settings_header', 'quizaccess_watermark'));
+        $mform->insertElementBefore($header, 'security');
+
+        $option = $mform->createElement(
+            'selectyesno',
+            'watermark_enabled',
+            get_string('settings_watermark_enable', 'quizaccess_watermark'),
+        );
+        $mform->setDefault('watermark_enabled', 0);
+        $mform->insertElementBefore($option, 'security');
     }
 
     /**
-     * Save any submitted settings when the quiz settings form is submitted. This
-     * is called from {@link quiz_after_add_or_update()} in lib.php.
-     * @param object $quiz the data from the quiz form, including $quiz->id
-     *      which is the id of the quiz being saved.
+     * @inheritDoc
+     * @throws coding_exception
+     * @throws dml_exception
      */
-    public static function save_settings($quiz) {
+    public static function save_settings($quiz): void {
         global $DB;
 
         $context = context_module::instance($quiz->coursemodule);
@@ -123,7 +117,7 @@ class quizaccess_watermark extends access_rule_base {
 
         $record = new stdClass();
         $record->quizid = $quiz->id;
-        $record->watermark = $quiz->watermark_enabled;
+        $record->watermark = $quiz->watermark_enabled ?? false;
 
         if (!$record->watermark) {
             $DB->delete_records('quizaccess_watermark', ['quizid' => $quiz->id]);
@@ -140,38 +134,18 @@ class quizaccess_watermark extends access_rule_base {
     }
 
     /**
-     * Delete any rule-specific settings when the quiz is deleted. This is called
-     * from {@link quiz_delete_instance()} in lib.php.
-     * @param object $quiz the data from the database, including $quiz->id
-     *      which is the id of the quiz being deleted.
-     * @since Moodle 2.7.1, 2.6.4, 2.5.7
+     * @inheritDoc
+     * @throws dml_exception
      */
-    public static function delete_settings($quiz) {
+    public static function delete_settings($quiz): void {
         global $DB;
         $DB->delete_records('quizaccess_watermark', ['quizid' => $quiz->id]);
     }
 
     /**
-     * Return the bits of SQL needed to load all the settings from all the access
-     * plugins in one DB query. The easiest way to understand what you need to do
-     * here is probalby to read the code of {@link quiz_access_manager::load_settings()}.
-     *
-     * If you have some settings that cannot be loaded in this way, then you can
-     * use the {@link get_extra_settings()} method instead, but that has
-     * performance implications.
-     *
-     * @param int $quizid the id of the quiz we are loading settings for. This
-     *     can also be accessed as quiz.id in the SQL. (quiz is a table alisas for {quiz}.)
-     * @return array with three elements:
-     *     1. fields: any fields to add to the select list. These should be alised
-     *        if neccessary so that the field name starts the name of the plugin.
-     *     2. joins: any joins (should probably be LEFT JOINS) with other tables that
-     *        are needed.
-     *     3. params: array of placeholder values that are needed by the SQL. You must
-     *        used named placeholders, and the placeholder names should start with the
-     *        plugin name, to avoid collisions.
+     * @inheritDoc
      */
-    public static function get_settings_sql($quizid) {
+    public static function get_settings_sql($quizid): array {
         return [
             'watermark.watermark AS watermark_enabled',
             'LEFT JOIN {quizaccess_watermark} watermark ON watermark.quizid = quiz.id',

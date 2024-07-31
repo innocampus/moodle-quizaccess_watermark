14c2260ea75c950bf192a0a82821b7b0b150d0d5
Daniil Fajnberg <d.fajnberg@tu-berlin.de>
2024-07-31 12:07:39 +0200

    Specify Moodle 4.3 as a requirement (closes #6)

--- version.php
+++ version.php
@@ -25,6 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2021030300;
-$plugin->requires = 2020061504;
+$plugin->requires = 2023100900;  // Moodle 4.3.0
+$plugin->supported = [403, 404];
 $plugin->component = 'quizaccess_watermark';
 $plugin->maturity = MATURITY_ALPHA;
